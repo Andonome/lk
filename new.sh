981778a89150eed8c06028ab189df3dd604f1c57
Malin Freeborn <malinfreeborn@posteo.net>
2023-06-27 23:06:05 +0200

    add new file script

---
+++ new.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo Select a category
+
+category="$(find . -type d -printf '%P\n' | fzy)"
+
+[ ! -d "$category" ] && mkdir "$category"
+
+echo Select a name
+
+read name
+
+filePath="$category/$name.md"
+
+tagsList="$(echo \"$category | sed 's#\/#", "#g')\""
+
+[ -e "$filePath" ] && $EDITOR $filePath && exit 0
+
+echo "---
+title: \"$name\"
+tags: [ $tagsList ]
+---
+
+" > "$filePath"
+
+$EDITOR "$filePath"
+
